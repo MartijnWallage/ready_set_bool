1be89d274e04906af3d5b30f400d9f35c84fa020
Martijn Wallage <martijnwallage@gmail.com>
2026-05-14 23:42:07 +0100

    Ex07 fixed bug

--- ex07.py
+++ ex07.py
@@ -11,23 +11,24 @@ class Var:
     def __hash__(self):
         return hash(self.name)
 
+    def __repr__(self):
+        return f"{self.name}" + ("!" if not self.sign else "")
 
-def unit_propagate(clauses: list[list[Var]], assignment: dict[str, bool]) -> dict[str, bool] | None:
+
+def unit_propagate(clauses: list[list[Var]], assignment: dict[str, bool]) -> bool:
     assignment = assignment
     for clause in clauses:
         if len(clause) == 1:
             var = clause[0]
             if var.name in assignment and assignment[var.name] != var.sign:
-                print(f"Clauses: {clauses}")
-                print(f"{var}.{var.sign} does not agree with {assignment}")
-                return None
+                return False
             assignment[var.name] = var.sign
-    return assignment
+    return True
 
 
-def pure_literal_assign(clauses: list[list[Var]], assignment: dict[str, bool]) -> dict[str, bool]:
+def pure_literal_assign(clauses: list[list[Var]], assignment: dict[str, bool]):
     trial_assign: dict[str, bool] = {}
-    ban_list: set[str] = set()
+    ban_list: set[str] = set(assignment.keys())
     for clause in clauses:
         for var in clause:
             if var.name not in ban_list:
@@ -36,21 +37,19 @@ def pure_literal_assign(clauses: list[list[Var]], assignment: dict[str, bool]) -
                     ban_list.add(var.name)
                 else:
                     trial_assign[var.name] = var.sign
-    return assignment | trial_assign
+    assignment |= trial_assign
 
 
-def simplify(clauses: list[list[Var]], assignment: dict[str, bool]) -> list[list[Var]]:
-    simplified = clauses[:]
-    for clause in simplified[:]:
+def simplify(clauses: list[list[Var]], assignment: dict[str, bool]) -> None:
+    for clause in clauses[:]:
         for var in clause[:]:
             if var.name not in assignment:
                 continue
             if assignment[var.name] == var.sign:
-                simplified.remove(clause)
+                clauses.remove(clause)
                 break
             else:
                 clause.remove(var)
-    return simplified
 
 
 def pick_next_literal(clauses: list[list[Var]], assignment: list[str, bool]) -> str:
@@ -66,33 +65,13 @@ def dpll(clauses: list[list[Var]], assignment: dict[str, bool]) -> bool:
     Algorithm DPLL
         Input: A set of prop Φ.
         Output: A truth value indicating whether Φ is satisfiable.
-    function DPLL(Φ)
-        // unit propagation:
-        while there is a unit clause {l} in Φ do
-            Φ ← unit-propagate(l, Φ);
-        // pure literal elimination:
-        while there is a literal l that occurs pure in Φ do
-            Φ ← pure-literal-assign(l, Φ);
-        // stopping conditions:
-        if Φ is empty then
-            return true;
-        if Φ contains an empty clause then
-            return false;
-        // DPLL procedure:
-        l ← choose-literal(Φ);
-        return DPLL(Φ ∧ {l}) or DPLL(Φ ∧ {¬l});
     """
-    old_clauses = clauses[:]
     # unit-propagate:
-    assignment = unit_propagate(clauses, assignment)
-    if assignment is None:
+    if not unit_propagate(clauses, assignment):
         return False
-    print(f"assignment after unit-propagate: {assignment}")
     # pure-literal-assign:
-    assignment = pure_literal_assign(clauses, assignment)
-    print(f"assignment after pure-literal-assign: {assignment}")
-    clauses = simplify(clauses, assignment)
-    print(f"Formula after simplify: {clauses}")
+    pure_literal_assign(clauses, assignment)
+    simplify(clauses, assignment)
 
     # Test validity
     if clauses == []:
@@ -101,9 +80,10 @@ def dpll(clauses: list[list[Var]], assignment: dict[str, bool]) -> bool:
         if clause == []:
             return False
 
-    next = pick_next_literal(old_clauses, assignment)
-    print(f"Next is {next}")
-    return dpll(old_clauses, assignment | {next: True}) or dpll(old_clauses, assignment | {next: False})
+    clauses_cpy = [clause[:] for clause in clauses]  # deep copy for backtracking
+    assignment_cpy = assignment.copy() # deep copy for backtracking
+    next = pick_next_literal(clauses, assignment)
+    return dpll(clauses, assignment | {next: True}) or dpll(clauses_cpy, assignment_cpy | {next: False})
 
 
 def split_clauses(formula: str) -> list[list[Var]]:
@@ -135,7 +115,6 @@ def sat(formula: str) -> bool:
     """
     cnf = conjunctive_normal_form(formula)
     clauses = split_clauses(cnf)
-    print(f"Clauses: {clauses}")
     return dpll(clauses=clauses, assignment={})
 
 def sat_brute_force(formula: str) -> bool:
