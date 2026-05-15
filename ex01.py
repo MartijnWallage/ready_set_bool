e4d66f2929d7cfd7b47ec3e2724ca4d629297dba
Martijn Wallage <martijnwallage@gmail.com>
2026-05-15 14:46:59 +0100

    Add complexity docstring

--- ex01.py
+++ ex01.py
@@ -2,6 +2,14 @@ from ex00 import adder
 
 
 def multiplier(a: int, b: int) -> int:
+    """
+    Bitwise multiplication.
+    Subject says time and space complexity should be O(1).
+    But that makes no sense: time and space needed for 
+    multiplication is obviously dependent on the size of the input.
+    Actual time complexity: O(logn)
+    Space complexity: O(n)
+    """
     result = 0
     i = 0
     while b >> i > 0:
